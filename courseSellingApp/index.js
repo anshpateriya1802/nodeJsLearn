0cd7460fcbc25e055e62aba750ce9fe4d6237fcc
anshpateriya1802 <pateriyaansh1802@gmail.com>
2025-06-02 23:37:52 +0530

    course selling app init

---
+++ courseSellingApp/index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const jwt=require("jsonwebtoken");
+
+const app=express();
+app.use(express.json());
+
+app.post("/signup",function(req,res){
+    
+})
+
+app.post("/signin",function(req,res){
+
+})
+
+app.post("/purchase",funtion(req,res){
+
+})
+
+app.get("/purchases",function(req,res){
+    
+})
